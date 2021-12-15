2cf15fe15afee4c3a85a2f6dda8c0f76e6ea2891
nutunabe <baiytaan@gmail.com>
2021-12-15 15:51:03 +0900

    l15

--- spring-mvc-app1/src/main/java/ru/nutunabe/springcourse/HelloController.java
+++ spring-mvc-app1/src/main/java/ru/nutunabe/springcourse/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-	
-	@GetMapping("/hello-world")
+
+	@GetMapping("/1")
 	public String sayHello() {
 		return "hello_world";
 	}
